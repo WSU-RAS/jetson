e7acae596c9ccfddb2674b43087e54571b4099cf
Garrett Wilson <g@floft.net>
2018-03-08 19:20:48 +0000

    Use trajectory to zero the pan tilt
    Previously it wouldn't always zero it. It might zero the tilt but not
    the pan.

--- src/zero_pan_tilt.py
+++ src/zero_pan_tilt.py
@@ -4,32 +4,61 @@ Sets the head pan/tilt to a particular position
 
 This is used to initialize it on start. Later on maybe it will track the
 person, but for now it'll be fixed.
+
+Based on:
+http://sdk.rethinkrobotics.com/wiki/Joint_Trajectory_Client_-_Code_Walkthrough
 """
+import sys
 import rospy
-from std_msgs.msg import Float64
+import argparse
+import actionlib
+from copy import copy
+from trajectory_msgs.msg import JointTrajectoryPoint
+from control_msgs.msg import FollowJointTrajectoryAction, \
+        FollowJointTrajectoryGoal
 
-def panTilt():
-    pan = rospy.Publisher('/head_pan_joint/command', Float64, queue_size=2)
-    tilt = rospy.Publisher('/head_tilt_joint/command', Float64, queue_size=2)
+class Trajectory(object):
+    def __init__(self):
+        ns = 'head_controller/'
+        self._client = actionlib.SimpleActionClient(
+            ns + "follow_joint_trajectory",
+            FollowJointTrajectoryAction,
+        )
+        self._goal = FollowJointTrajectoryGoal()
+        server_up = self._client.wait_for_server(timeout=rospy.Duration(10.0))
+        if not server_up:
+            rospy.logerr("Timed out waiting for Joint Trajectory"
+                         " Action Server to connect. Start arbotix node.")
+            rospy.signal_shutdown("Timed out waiting for Action Server")
+            sys.exit(1)
+        self.clear()
+    def add_point(self, positions, time):
+        point = JointTrajectoryPoint()
+        point.positions = copy(positions)
+        point.time_from_start = rospy.Duration(time)
+        self._goal.trajectory.points.append(point)
+    def start(self):
+        self._goal.trajectory.header.stamp = rospy.Time.now()
+        self._client.send_goal(self._goal)
+    def stop(self):
+        self._client.cancel_goal()
+    def wait(self, timeout=15.0):
+        self._client.wait_for_result(timeout=rospy.Duration(timeout))
+    def result(self):
+        return self._client.get_result()
+    def clear(self):
+        self._goal = FollowJointTrajectoryGoal()
+        self._goal.trajectory.joint_names = ["head_" + joint + "_joint" for joint in \
+            ["pan", "tilt"]]
+def main():
     rospy.init_node('pan_tilt')
-    hz = 10
-    seconds = 10 # exit after this many seconds (takes some time for Arbotix to launch)
-    rate = rospy.Rate(hz) # Hz
+    traj = Trajectory()
+    rospy.on_shutdown(traj.stop)
 
-    i = 0
     while not rospy.is_shutdown():
-        pan.publish(0)
-        #tilt.publish(0.25)
-        tilt.publish(0.7)
-        rate.sleep()
+        traj.add_point([0.0, 0.7], 1.0) # pan, tilt, time to get there
+        traj.start()
+        traj.wait(5.0)
 
-        # Run only briefly
-        i += 1
-        if i >hz*seconds: 
-            break
-
-if __name__ == '__main__':
-    try:
-        panTilt()
-    except rospy.ROSInterruptException:
-        pass
+if __name__ == "__main__":
+    main()
